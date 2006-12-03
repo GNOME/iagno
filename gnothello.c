5ea7492c9abc71b914deb82d8dabc60f0dd5a22a
Andreas Røsdal <andreasr@src.gnome.org>
2006-12-03 12:50:14 +0000

    Bug #381751 – Iagno should not call gnome_score_init().

--- gnothello.c
+++ gnothello.c
@@ -885,8 +885,6 @@ main (int argc, char **argv)
   GnomeProgram *program;
   GOptionContext *context;
 
-  gnome_score_init ("iagno");
-
   bindtextdomain (GETTEXT_PACKAGE, GNOMELOCALEDIR);
   bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
   textdomain (GETTEXT_PACKAGE);
