5665f3ba63ae7b370ebddeaf94902f38b6fa74bc
Arnaud Bonatti <arnaud.bonatti@gmail.com>
2019-02-04 14:04:22 +0100

    Make post-install work if datadir set.
    As suggested by Jan Tojnar, done by
    copying commit gnome-2048@84ad3134.
    
    See !4.

--- build-aux/meson_post_install.py
+++ build-aux/meson_post_install.py
@@ -2,13 +2,27 @@
 
 from os import environ, path
 from subprocess import call
+import sys
+
+if len(sys.argv) < 4:
+    sys.exit("usage: meson_post_install.py <icondir> <schemadir> <appsdir>")
+
+icon_cache_dir  = sys.argv[1]
+schemadir       = sys.argv[2]
+appsdir         = sys.argv[3]
 
 if not environ.get('DESTDIR', ''):
-    PREFIX = environ.get('MESON_INSTALL_PREFIX', '/usr/local')
-    DATA_DIR = path.join(PREFIX, 'share')
     print('Updating icon cache...')
-    call(['gtk-update-icon-cache', '-qtf', path.join(DATA_DIR, 'icons/hicolor')])
+    if not os.path.exists(icon_cache_dir):
+        os.makedirs(icon_cache_dir)
+    call(['gtk-update-icon-cache', '-qtf', icon_cache_dir])
+
     print("Compiling new schemas")
-    call(["glib-compile-schemas", path.join(DATA_DIR, 'glib-2.0/schemas')])
+    if not os.path.exists(schemadir):
+        os.makedirs(schemadir)
+    call(["glib-compile-schemas", schemadir])
+
     print("Updating desktop database")
-    call(["update-desktop-database", path.join(DATA_DIR, 'applications')])
+    if not os.path.exists(appsdir):
+        os.makedirs(appsdir)
+    call(["update-desktop-database", appsdir])
